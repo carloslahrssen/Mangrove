1d804f808185023f90c2040709f112cc864c80f8
Diego <Dgomez.1730@hotmail.com>
2018-01-20 18:37:59 -0500

    Testing dropdown

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => {
-	res.sendFile('/Users/dieg/code/Mangrove/index.html');
+	res.sendFile('/Users/dieg/code/Mangrove/room.html');
 });
 
 app.listen(3000);
