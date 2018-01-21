cab8849a21d1651c0a45651ca9eb7d73fc4a078c
carloslahrssen <carloslahrssen@gmail.com>
2018-01-21 04:00:23 -0500

    Added cool features

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(8080);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/index.html');
@@ -9,6 +15,4 @@ app.get('/room',(req,res)=>{
 	res.sendFile(__dirname+'/room.html');
 });
 
-
-app.listen(3000);
 
