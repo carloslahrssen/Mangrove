e55750da834946827fa9628b379b88f997ce140c
Diego <Dgomez.1730@hotmail.com>
2018-01-21 02:27:11 -0500

    Before styling

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req,res) => {
-	res.sendFile(__dirname + '/room.html');
+	res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/room',(req,res)=>{
+	res.sendFile(__dirname+'/room.html');
+});
+
 
 app.listen(3000);
 
